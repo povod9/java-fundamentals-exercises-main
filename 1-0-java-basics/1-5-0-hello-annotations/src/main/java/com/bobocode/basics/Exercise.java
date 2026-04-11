46879f0c3abe3fd214d7d2f533d100aaa46de6c3
povod9 <nazarpovod113@gmail.com>
2026-04-11 13:45:08 +0200

    completed task 1-0-java-basic/1-5-0-hello-annotation

--- 1-0-java-basics/1-5-0-hello-annotations/src/main/java/com/bobocode/basics/Exercise.java
+++ 1-0-java-basics/1-5-0-hello-annotations/src/main/java/com/bobocode/basics/Exercise.java
@@ -5,8 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Exercise {
-    String value();
+    String value() default "Exercise";
+    Level complexityLevel() default Level.BASIC;
 }
