9e9d839a977109c56b79bb4155c0e9eb556b525e
povod9 <nazarpovod113@gmail.com>
2026-04-11 19:07:09 +0200

    Implement custome stack

---
+++ 2-0-data-structures-and-algorithms/2-2-2-stack/src/main/java/com/bobocode/cs/LinkedStack.java
@@ -0,0 +1,101 @@
+package com.bobocode.cs;
+
+import com.bobocode.cs.exception.EmptyStackException;
+import com.bobocode.util.ExerciseNotCompletedException;
+
+import javax.sound.sampled.Line;
+
+
+/**
+ * {@link LinkedStack} is a stack implementation that is based on singly linked generic nodes.
+ * A node is implemented as inner static class {@link Node<T>}.
+ * <p><p>
+ * <strong>TODO: to get the most out of your learning, <a href="https://www.bobocode.com">visit our website</a></strong>
+ * <p>
+ *
+ * @param <T> generic type parameter
+ * @author Taras Boychuk
+ * @author Serhii Hryhus
+ */
+public class LinkedStack<T> implements Stack<T> {
+    private Node<T> head;
+    private int size;
+
+    public static class Node<T>{
+        T element;
+        Node<T> next;
+    }
+    /**
+     * This method creates a stack of provided elements
+     *
+     * @param elements elements to add
+     * @param <T>      generic type
+     * @return a new stack of elements that were passed as method parameters
+     */
+    public static <T> LinkedStack<T> of(T... elements) {
+        LinkedStack<T> stack = new LinkedStack<>();
+
+        for (T element : elements){
+            stack.push(element);
+        }
+        return stack;
+    }
+
+    /**
+     * The method pushes an element onto the top of this stack. This has exactly the same effect as:
+     * addElement(item)
+     *
+     * @param element elements to add
+     */
+    @Override
+    public void push(T element) {
+        if(element == null){
+            throw new NullPointerException();
+        }
+        Node<T> node = new Node<>();
+        node.element = element;
+        node.next = head;
+        head = node;
+        size++;
+    }
+
+    /**
+     * This method removes the object at the top of this stack
+     * and returns that object as the value of this function.
+     *
+     * @return The object at the top of this stack
+     * @throws EmptyStackException - if this stack is empty
+     */
+    @Override
+    public T pop() {
+        if(size == 0){
+            throw  new EmptyStackException();
+        }
+        T element = head.element;
+        head = head.next;
+        size--;
+        return element;
+    }
+
+    /**
+     * Returns the number of elements in the stack
+     *
+     * @return number of elements
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Checks if a stack is empty
+     *
+     * @return {@code true} if a stack is empty, {@code false} otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+}
