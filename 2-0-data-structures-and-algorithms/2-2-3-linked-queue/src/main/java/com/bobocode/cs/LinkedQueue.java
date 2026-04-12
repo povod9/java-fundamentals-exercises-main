ea80c47a701202c36b2d2825f1a17e78677193b4
povod9 <nazarpovod113@gmail.com>
2026-04-12 14:52:33 +0200

    Complete task with linked-queue

---
+++ 2-0-data-structures-and-algorithms/2-2-3-linked-queue/src/main/java/com/bobocode/cs/LinkedQueue.java
@@ -0,0 +1,84 @@
+package com.bobocode.cs;
+
+import com.bobocode.util.ExerciseNotCompletedException;
+
+/**
+ * {@link LinkedQueue} implements FIFO {@link Queue}, using singly linked nodes. Nodes are stores in instances of nested
+ * class Node. In order to perform operations {@link LinkedQueue#add(Object)} and {@link LinkedQueue#poll()}
+ * in a constant time, it keeps to references to the head and tail of the queue.
+ * <p><p>
+ * <strong>TODO: to get the most out of your learning, <a href="https://www.bobocode.com">visit our website</a></strong>
+ * <p>
+ *
+ * @param <T> a generic parameter
+ * @author Taras Boychuk
+ * @author Ivan Virchenko
+ */
+public class LinkedQueue<T> implements Queue<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+
+    public static class Node<T>{
+        T element;
+        Node<T> next;
+    }
+    /**
+     * Adds an element to the end of the queue.
+     *
+     * @param element the element to add
+     */
+    public void add(T element) {
+        Node<T> node = new Node<>();
+        node.element = element;
+        if(size == 0) {
+            head = node;
+            tail = node;
+            size++;
+        }else {
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+    }
+
+    /**
+     * Retrieves and removes queue head.
+     *
+     * @return an element that was retrieved from the head or null if queue is empty
+     */
+    public T poll() {
+        if(size == 0){
+            return null;
+        }
+        T result = head.element;
+        head = head.next;
+        size--;
+        if(head == null || size == 0){
+            tail=null;
+        }
+        return result;
+    }
+
+    /**
+     * Returns a size of the queue.
+     *
+     * @return an integer value that is a size of queue
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return {@code true} if the queue is empty, returns {@code false} if it's not
+     */
+    public boolean isEmpty() {
+        if (size == 0){
+            return true;
+        }
+        return false;
+    }
+}
