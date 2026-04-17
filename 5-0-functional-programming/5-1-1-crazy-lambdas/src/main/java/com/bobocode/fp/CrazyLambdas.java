4b1f314b1692f4a8c66b882d7b79777af0cf7f36
povod9 <nazarpovod113@gmail.com>
2026-04-17 14:54:43 +0200

    Complete task crazy-lambdas

---
+++ 5-0-functional-programming/5-1-1-crazy-lambdas/src/main/java/com/bobocode/fp/CrazyLambdas.java
@@ -0,0 +1,331 @@
+package com.bobocode.fp;
+
+import com.bobocode.util.ExerciseNotCompletedException;
+
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.*;
+
+/**
+ * {@link CrazyLambdas} is an exercise class. Each method returns a functional interface and it should be implemented
+ * using either lambda or a method reference. Every method that is not implemented yet throws
+ * {@link ExerciseNotCompletedException}.
+ * <p>
+ * TODO: remove exception and implement each method of this class using lambda or method reference
+ * <p><p>
+ * <strong>TODO: to get the most out of your learning, <a href="https://www.bobocode.com">visit our website</a></strong>
+ * <p>
+ *
+ * @author Taras Boychuk
+ */
+public class CrazyLambdas {
+
+    /**
+     * Returns {@link Supplier} that always supply "Hello"
+     *
+     * @return a string supplier
+     */
+    public static Supplier<String> helloSupplier() {
+        // By Anonymous Class
+//        Supplier<String> hello = new Supplier<String>() {
+//            @Override
+//            public String get() {
+//                return "Hello";
+//            }
+//        };
+//        return hello;
+
+        // Or By Lambda
+        Supplier<String> hello = () -> "Hello";
+        return hello;
+
+    }
+
+    /**
+     * Returns a {@link Predicate} of string that checks if string is empty
+     *
+     * @return a string predicate
+     */
+    public static Predicate<String> isEmptyPredicate() {
+        // Anonymous Class
+//        Predicate<String> isEmpString = new Predicate<String>() {
+//            @Override
+//            public boolean test(String s) {
+//                return s.isEmpty();
+//            }
+//        };
+//        return isEmpString;
+
+
+
+        //          Lambda
+//        Predicate<String> isEmpString = result -> result.isEmpty();
+//        return isEmpString;
+
+
+        // Method Reference
+        return String::isEmpty;
+     }
+
+    /**
+     * Return a {@link Function} that accepts {@link String} and returns that string repeated n time, where n is passed
+     * as function argument
+     *
+     * @return function that repeats Strings
+     */
+    public static BiFunction<String, Integer, String> stringMultiplier() {
+
+        // Anonymous Class
+
+//        BiFunction<String, Integer, String> repString = new BiFunction<String, Integer, String>() {
+//            @Override
+//            public String apply(String s, Integer integer) {
+//                String result = "";
+//                for (int i = 0; i < integer; i++) {
+//                    result += s;
+//                }
+//                return result;
+//            }
+//        };
+//        return repString;
+
+        // Lambda
+//        BiFunction<String, Integer, String> repString = (s, n) -> s.repeat(n);
+//        return repString;
+
+        // Method Reference
+        return String::repeat;
+    }
+
+    /**
+     * Returns a {@link Function} that converts a {@link BigDecimal} number into a {@link String} that start with
+     * a dollar sign and then gets a value
+     *
+     * @return function that converts adds dollar sign
+     */
+    public static Function<BigDecimal, String> toDollarStringFunction() {
+
+        //Anonymous class
+        //        Function<BigDecimal, String> result = new Function<BigDecimal, String>() {
+//            @Override
+//            public String apply(BigDecimal bigDecimal) {
+//                return "$" + bigDecimal;
+//            }
+//        };
+//        return result;
+
+        // Lambda
+
+        return b -> "$" + b;
+
+
+    }
+
+    /**
+     * Receives two parameter that represent a range and returns a {@link Predicate<String>} that verifies if string
+     * length is in the specified range. E.g. min <= length < max
+     *
+     * @param min min length
+     * @param max max length
+     * @return a string predicate
+     */
+    public static Predicate<String> lengthInRangePredicate(int min, int max) {
+        Predicate<String> result = s -> s.length() >= min && s.length() < max;
+        return result;
+    }
+
+    /**
+     * Returns a {@link Supplier} of random integers
+     *
+     * @return int supplier
+     */
+    public static IntSupplier randomIntSupplier() {
+        Random random = new Random();
+
+        //Method Reference
+//        IntSupplier result = random::nextInt;
+        // Lambda
+        IntSupplier result = () -> random.nextInt();
+        return result;
+    }
+
+
+    /**
+     * Returns an {@link IntUnaryOperator} that receives an int as a bound parameter, and returns a random int
+     *
+     * @return int operation
+     */
+    public static IntUnaryOperator boundedRandomIntSupplier() {
+        Random random = new Random();
+
+        IntUnaryOperator result = o -> random.nextInt();
+        return result;
+    }
+
+    /**
+     * Returns {@link IntUnaryOperator} that calculates an integer square
+     *
+     * @return square operation
+     */
+    public static IntUnaryOperator intSquareOperation() {
+        return o -> o * o;
+    }
+
+    /**
+     * Returns a {@link LongBinaryOperator} sum operation.
+     *
+     * @return binary sum operation
+     */
+    public static LongBinaryOperator longSumOperation() {
+        return Long::sum;
+    }
+
+    /**
+     * Returns a {@link ToIntFunction<String>} that converts string to integer.
+     *
+     * @return string to int converter
+     */
+    public static ToIntFunction<String> stringToIntConverter() {
+        return Integer::parseInt;
+    }
+
+    /**
+     * Receives int parameter n, and returns a {@link Supplier} that supplies {@link IntUnaryOperator}
+     * that is a function f(x) = n * x
+     *
+     * @param n a multiplier
+     * @return a function supplier
+     */
+    public static Supplier<IntUnaryOperator> nMultiplyFunctionSupplier(int n) {
+        Supplier<IntUnaryOperator> result = () -> (operand) ->  operand * n;
+        return result;
+    }
+
+    /**
+     * Returns a {@link UnaryOperator} that accepts str to str function and returns the same function composed with trim
+     *
+     * @return function that composes functions with trim() function
+     */
+    public static UnaryOperator<Function<String, String>> composeWithTrimFunction() {
+        return f -> f.compose(String::trim);
+    }
+
+    /**
+     * Receives a {@link Runnable} parameter, and returns a {@link Supplier<Thread>}. The thread will be started only
+     * when you call supplier method {@link Supplier#get()}
+     *
+     * @param runnable the code you want to tun in new thread
+     * @return a thread supplier
+     */
+    public static Supplier<Thread> runningThreadSupplier(Runnable runnable) {
+        Thread t = new Thread(runnable);
+        t.start();
+        return () -> t;
+    }
+
+    /**
+     * Returns a {@link Consumer} that accepts {@link Runnable} as a parameter and runs in a new thread.
+     *
+     * @return a runnable consumer
+     */
+    public static Consumer<Runnable> newThreadRunnableConsumer() {
+        return Runnable::run;
+    }
+
+    /**
+     * Returns a {@link Function} that accepts an instance of {@link Runnable} and returns a {@link Supplier} of a
+     * started {@link Thread} that is created from a given {@link Runnable}
+     *
+     * @return a function that transforms runnable into a thread supplier
+     */
+    public static Function<Runnable, Supplier<Thread>> runnableToThreadSupplierFunction() {
+
+        // Not Completed
+        throw new ExerciseNotCompletedException();
+    }
+
+    /**
+     * Returns a {@link BiFunction} that has two parameters. First is {@link IntUnaryOperator} which is some integer function.
+     * Second is {@link IntPredicate} which is some integer condition. And the third is {@link IntUnaryOperator} which is
+     * a new composed function that uses provided predicate (second parameter of binary function) to verify its input
+     * parameter. If predicate returns {@code true} it applies a provided integer function
+     * (first parameter of binary function) and returns a result value, otherwise it returns an element itself.
+     *
+     * @return a binary function that receiver predicate and function and compose them to create a new function
+     */
+    public static BiFunction<IntUnaryOperator, IntPredicate, IntUnaryOperator> functionToConditionalFunction() {
+//        BiFunction<IntUnaryOperator, IntPredicate, IntUnaryOperator> result =
+//                (intUnaryOperator, intPredicate) ->  operand -> intUnaryOperator.applyAsInt(operand);
+//        return result;
+
+        // Not Completed
+        throw new ExerciseNotCompletedException();
+    }
+
+    /**
+     * Returns a {@link BiFunction} which first parameter is a {@link Map} where key is a function name, and value is some
+     * {@link IntUnaryOperator}, and second parameter is a {@link String} which is a function name. If the map contains a
+     * function by a given name then it is returned by high order function otherwise an identity() is returned.
+     *
+     * @return a high-order function that fetches a function from a function map by a given name or returns identity()
+     */
+    public static BiFunction<Map<String, IntUnaryOperator>, String, IntUnaryOperator> functionLoader() {
+//        BiFunction<Map<String, IntUnaryOperator>, String, IntUnaryOperator> result =
+//                ((stringIntUnaryOperatorMap, s) -> stringIntUnaryOperatorMap.get(s));
+//        return result;
+        // Not Completed
+        throw new ExerciseNotCompletedException();
+    }
+
+    /**
+     * Returns a comparator of type T that is comparing values extracted using the provided mapper function.
+     * <p>
+     * E.g. imagine you need to compare accounts by their balance values.
+     * <pre>{@code
+     * Comparator<Account> balanceComparator = comparing(Account::getBalance);
+     * }</pre>
+     * <p>
+     * PLEASE NOTE, that @{@link Comparator} is a functional interface, and you should manually write a lambda expression
+     * to implement it.
+     *
+     * @param mapper a mapper function that allows to map an object to a comparable value
+     * @return a comparator instance
+     */
+    public static <T, U extends Comparable<? super U>> Comparator<T> comparing(Function<? super T, ? extends U> mapper) {
+        // Not Completed
+        throw new ExerciseNotCompletedException();
+    }
+
+    /**
+     * Returns a comparator of type T that uses a provided comparator to compare objects, and only if they are equal
+     * it's comparing values extracted using the provided mapper function.
+     * <p>
+     * E.g. suppose you want to compare accounts by balance, but in case two people have the same balance you want to
+     * compare their first names:
+     * <pre>{@code
+     * Comparator<Account> accountComparator = thenComparing(balanceComparator, Account::getFirstName);
+     * }</pre>
+     * <p>
+     *
+     * @param comparator an initial comparator
+     * @param mapper     a mapper function that is used to extract values when initial comparator returns zero
+     * @return a comparator instance
+     */
+    public static <T, U extends Comparable<? super U>> Comparator<T> thenComparing(
+            Comparator<? super T> comparator, Function<? super T, ? extends U> mapper) {
+        // Not Completed 
+        throw new ExerciseNotCompletedException();
+    }
+
+    /**
+     * Returns {@link Supplier} of {@link Supplier} of {@link Supplier} of {@link String} "WELL DONE!".
+     *
+     * @return a supplier instance
+     */
+    public static Supplier<Supplier<Supplier<String>>> trickyWellDoneSupplier() {
+        return () -> () -> () -> "WELL DONE!";
+    }
+}
+
